f2d26bdf4f38c6082c88943bb50211f6ceaf06be
Clooooode <jackey8616@gmail.com>
2021-02-15 00:19:13 +0800

    feat: Add run migrations script
    Add typeorm script for runMigrations.
    Add minimist package to resolve arguments.
    Exclude scripts folder since typeorm script is written in typescript.

---
+++ src/config/index.ts
@@ -0,0 +1,26 @@
+// src/config/index.ts
+import { Connection, ConnectionOptions, createConnection } from 'typeorm';
+import * as prodConfig from './production';
+import * as devConfig from './dev';
+
+function verboseParse(verbose: any): boolean | ['schema'] | 'all' {
+  switch (verbose) {
+    case 0:
+      return false;
+    case 1:
+      return ['schema'];
+    case 2:
+    default:
+      return 'all';
+  }
+}
+
+export async function runMigrations(verbose: any): Promise<void> {
+  const config: any = process.env.MODE === 'production' ? prodConfig : devConfig;
+  config.default.logging = verboseParse(verbose);
+  const conn: Connection = await createConnection(config.default as ConnectionOptions);
+
+  await conn.runMigrations();
+
+  conn.close();
+}
